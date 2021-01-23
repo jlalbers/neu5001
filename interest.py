8226f33dd6d28ea1516934f3231818053d02fe38
jlalbers <albers.j@northeastern.edu>
2021-01-22 20:50:32 -0800

    Edited interest.py after pycodestyle

--- interest.py
+++ interest.py
@@ -1,17 +1,21 @@
 def main():
 
-    principle = float(input('Enter the initial savings balance: ')) #Prompts user to input the initial savings account balance
+    # # Prompts user to input the initial savings account balance
+    principle = float(input('Enter the initial savings balance: '))
 
-    rate = 0.06 #The annual interest rate of the account
-    n_yearly = 12 #Times per year the interest is compounded (monthly would be 12 times per year)
+    rate = 0.06  # The annual interest rate of the account
+    n_yearly = 12  # Times per year the interest is compounded (monthly would be 12)
 
-    month_1 = principle * (1.0 + (rate / n_yearly)) ** 1 #Calculates the balance after 1 month using compound interest formula
-    month_2 = principle * (1.0 + (rate / n_yearly)) ** 2 #Calculates the balance after 2 months using compound interest formula
-    month_3 = principle * (1.0 + (rate / n_yearly)) ** 3 #Calculates the balance after 3 months using compound interest formula
+    # Use compount interest formula to calculate monthly balances
+    month_1 = principle * (1.0 + (rate / n_yearly)) ** 1
+    month_2 = principle * (1.0 + (rate / n_yearly)) ** 2
+    month_3 = principle * (1.0 + (rate / n_yearly)) ** 3 
 
-    print('Initial Balance: ', principle) #Prints the initial balance
-    print('After One Month: ', month_1) #Prints the balance after one month
-    print('After Two Months: ', month_2) #Prints the balance after two months
-    print('After Three Months: ', month_3) #Prints the balance after three months
+    # Print the balances
+    print('Initial Balance: ', principle)
+    print('After One Month: ', month_1)
+    print('After Two Months: ', month_2)
+    print('After Three Months: ', month_3)
 
 main()
+
