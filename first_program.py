562b089f2ebfff01ffc4738f6176d06e6614e1d1
jlalbers <albers.j@northeastern.edu>
2021-01-22 22:34:03 -0800

    Commit Module 1 Lab

---
+++ first_program.py
@@ -0,0 +1,21 @@
+'''
+Jameson Albers
+CS 5001, Spring 2021
+Module 1 Lab: Getting Started
+
+Prints FUN in large letters.
+'''
+
+def main(): 
+
+    print('FFFFFFFFFF    UUU    UUU    NNN     NNN')
+    print('FFFFFFFFFF    UUU    UUU    NNNN    NNN')
+    print('FFF           UUU    UUU    NNNNN   NNN')
+    print('FFF           UUU    UUU    NNNNNN  NNN')
+    print('FFFFFF        UUU    UUU    NNN NNN NNN')
+    print('FFFFFF        UUU    UUU    NNN  NNNNNN')
+    print('FFF           UUU    UUU    NNN   NNNNN')
+    print('FFF            UUUUUUUU     NNN    NNNN')
+    print('FFF             UUUUUU      NNN     NNN')
+
+main()
